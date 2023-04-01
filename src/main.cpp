ca3ce25403cac6adcd511c4edb41d2c1b3bad03d
alexnavtt <alexnavtt@utexas.edu>
2023-04-01 00:47:58 -0500

    inverted second image

--- src/main.cpp
+++ src/main.cpp
@@ -33,23 +33,33 @@ int main(int argc, char* argv[]){
     K.biases[1] = 0;
     std::cout << "Kernel biases are " << K.biases[0] << " and " << K.biases[1] << "\n";
 
+    // Convert the byte image to float data
     my_cnn::SimpleMatrix<float> floating_point_image = input_image;
     floating_point_image /= 255.0; 
+
+    // Invert the second layer
+    auto im2 = floating_point_image.slice(1);
+    im2 = im2*-1 + 1; 
+    floating_point_image.subMatView({0, 0, 1}, {25, 25, 1}) = im2.data();
+
+    // Display both layers
     std::cout << "Input images: \n";
     K.setInputData(&floating_point_image);
     my_cnn::printImage(floating_point_image.slice(0));
     my_cnn::printImage(floating_point_image.slice(1));
 
+    // Apply the convolution kernel
     my_cnn::SimpleMatrix<float> convolved_image = K.convolve();
     my_cnn::printImage(convolved_image.slice(0));
     my_cnn::printImage(convolved_image.slice(1));
 
+    // Run a 2x2 mean pooling
     my_cnn::Pooling pool;
     pool.dim0 = 2;
     pool.dim1 = 2;
     pool.stride0 = 2;
     pool.stride1 = 2;
-    pool.type = my_cnn::MAX;
+    pool.type = my_cnn::AVG;
     my_cnn::SimpleMatrix<float> pooled_image = my_cnn::pooledMatrix(convolved_image, pool);
     my_cnn::printImage(pooled_image.slice(0));
     my_cnn::printImage(pooled_image.slice(1));
