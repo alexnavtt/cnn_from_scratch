2df40b2bb81dc4d10ce00e88335876f491656ef5
alexnavtt <alexnavtt@utexas.edu>
2023-03-31 21:03:18 -0500

    Added support for displaying images in the terminal

---
+++ include/cnn_from_scratch/imageUtil.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "cnn_from_scratch/SimpleMatrix.h"
+
+namespace my_cnn{
+    
+template <typename T>
+void printImageBWImpl(const SimpleMatrix<T>& M, T max_val){
+    for (size_t row = 0; row < M.dims().x; row++){
+        for (size_t col = 0; col < M.dims().y; col++){
+            const T& val = M(row, col, 0);
+            unsigned char code = std::min(static_cast<float>(val)/max_val * 255, 255.0f);
+            printf("\033[48;2;%d;%d;%dm  ", (int)code, (int)code, (int)code);
+        }
+        std::cout << "\033[0m\n";
+    }
+}
+
+template <typename T>
+void printImageColorImpl(const SimpleMatrix<T>& M, T max_val){
+    for (size_t row = 0; row < M.dims().x; row++){
+        for (size_t col = 0; col < M.dims().y; col++){
+            std::stringstream ss;
+            ss << "\033[48;2";
+            for (size_t channel = 0; channel < 3; channel++){
+                const T& val = M(row, col, channel);
+                unsigned char code = std::min(static_cast<float>(val)/max_val * 255, 255.0f);
+                ss << ";" << +code;
+            }
+            ss << "m   ";
+            std::cout << ss.str();
+        }
+        std::cout << "\033[0m\n";
+    }
+}
+
+template<typename T>
+void printImage(const SimpleMatrix<T>& M){
+    if constexpr (std::is_integral_v<T>){
+        if (M.dim(2) == 1)
+            printImageBWImpl<T>(M, std::numeric_limits<unsigned char>::max());
+        else if (M.dim(2) == 3)
+            printImageColorImpl<T>(M, std::numeric_limits<unsigned char>::max()); 
+    }else if (std::is_floating_point_v<T>){
+        if (M.dim(2) == 1)
+            printImageBWImpl<T>(M, 1.0);
+        else if (M.dim(2) == 3)
+            printImageColorImpl<T>(M, 1.0); 
+    }
+}
+
+} // namespace my_cnn
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,43 @@
-#include <iostream>
+#include <limits>
+#include <sstream>
+#include <cstring>
 #include <iomanip>
-#include <cnn_from_scratch/Kernel.h>
+#include <assert.h>
+#include <iostream>
+#include "cnn_from_scratch/imageUtil.h"
+#include "cnn_from_scratch/Kernel.h"
 
 int main(int argc, char* argv[]){
+
+    std::cout << "\033[38;2;0;0;0m" << "Black text\033[0m\n";
+
+    // Grayscale image for testing
+    my_cnn::SimpleMatrix<unsigned char> input_image({25, 25, 1});
+    
+    // Let's make it a 4 for fun
+    input_image.subMatView({ 5,  4,  0}, {10,  2,  1}) = 255;
+    input_image.subMatView({15,  4,  0}, { 2, 12,  1}) = 255;
+    input_image.subMatView({ 7, 12,  0}, {15,  1,  1}) = 255;
+    printImage(input_image);
+    std::cout << input_image << "\n";
+
+    // Convert it to a floating point representation
+    my_cnn::SimpleMatrix<float> other_image = input_image/255;
+    other_image.conditionallySet(0.5, my_cnn::SimpleMatrix<float>::GREATER, 0);
+    // other_image.subMatView() -= 0.25;
+    std::cout << other_image << "\n";
+    printImage(other_image);
+
+    // Radial Gradient
+    my_cnn::SimpleMatrix<float> radial_grad({25, 25, 3});
+    for (uint x = 0; x < radial_grad.dim(0); x++){
+        for (uint y = 0; y < radial_grad.dim(1); y++){
+            radial_grad(x, y, 0) = sqrt(x*x + y*y)/40.0f;
+            radial_grad(x, y, 1) = 0.5*float(x)/radial_grad.dim(0);
+            radial_grad(x, y, 2) = 0.5*float(y)/radial_grad.dim(1);
+        }
+    }
+    printImage(radial_grad);
 
     my_cnn::Kernel K({6, 6, 1}, 1);
     std::cout << "Kernel weights are\n" << K.weights << "\n";
