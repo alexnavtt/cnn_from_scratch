038495e3b17c3835506459a2481f8d4bb062a98a
alexnavtt <alexnavtt@utexas.edu>
2023-04-01 00:21:51 -0500

    Added normalization for visualization only

--- include/cnn_from_scratch/imageUtil.h
+++ include/cnn_from_scratch/imageUtil.h
@@ -6,10 +6,14 @@ namespace my_cnn{
     
 template <typename T>
 void printImageBWImpl(const SimpleMatrix<T>& M, T max_val){
+    T min = M.min();
+    T range = max_val - min;
     for (size_t row = 0; row < M.dims().x; row++){
         for (size_t col = 0; col < M.dims().y; col++){
             const T& val = M(row, col, 0);
-            unsigned char code = std::min(static_cast<float>(val)/max_val * 255, 255.0f);
+            float dist = max_val - val;
+            float mapped_val = max_val * (1 - dist/range);
+            unsigned char code = std::min(static_cast<float>(mapped_val)/max_val * 255, 255.0f);
             printf("\033[48;2;%d;%d;%dm  ", (int)code, (int)code, (int)code);
         }
         std::cout << "\033[0m\n";
