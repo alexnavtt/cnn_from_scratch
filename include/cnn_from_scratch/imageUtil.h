5ee260f208bf3eadd26e0258c4d7bdfd473333d9
alexnavtt <alexnavtt@utexas.edu>
2023-04-12 18:29:30 -0500

    Cleaned up a little

--- include/cnn_from_scratch/imageUtil.h
+++ include/cnn_from_scratch/imageUtil.h
@@ -40,11 +40,7 @@ void printImageColorImpl(const SimpleMatrix<T>& M, T max_val){
 
 template<typename T>
 void printImage(const SimpleMatrix<T>& M){
-    T max;
-    if constexpr (std::is_integral_v<T>)
-        max = std::numeric_limits<unsigned char>::max();
-    else
-        max = 1.0;
+    T max = (std::is_integral_v<T> ? std::numeric_limits<unsigned char>::max() : 1.0);
 
     if (M.dim(2) == 3)
         printImageColorImpl<T>(M, max); 
