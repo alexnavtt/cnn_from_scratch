58334d6d18e53c54fdbe4842e0233c60b65b3f03
alexnavtt <alexnavtt@utexas.edu>
2023-04-13 11:31:07 -0500

    Added an exception for layer size mismatch

--- include/cnn_from_scratch/exceptions.h
+++ include/cnn_from_scratch/exceptions.h
@@ -13,4 +13,12 @@ private:
     std::string msg_;
 };
 
+class ModelLayerException : public std::exception{
+public:
+    ModelLayerException(std::string msg = "") : msg_{msg} {}
+    const char* what() const noexcept {return msg_.c_str();}
+private:
+    std::string msg_;
+};
+
 } // namespace my_cnn
