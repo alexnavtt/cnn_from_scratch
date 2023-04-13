467a8cb9e88ab0593e49ccf9627c1dd10656a56b
alexnavtt <alexnavtt@utexas.edu>
2023-04-12 19:45:59 -0500

    Added activation functions and fixed indexing

--- include/cnn_from_scratch/Kernel.h
+++ include/cnn_from_scratch/Kernel.h
@@ -6,13 +6,22 @@
 #include "cnn_from_scratch/imageUtil.h"
 
 namespace my_cnn{
+
+enum ModelActivationFunction{
+    RELU,
+    SIGMOID,
+    LINEAR,
+    TANGENT,
+    LEAKY_RELU
+};
     
 class Kernel {
 public:
-    Kernel(dim3 dim, unsigned stride) :
-    dim_(dim),
-    weights(dim),
-    biases_(dim),
+    Kernel(dim3 filter_dim, unsigned num_filters, unsigned stride) :
+    dim_(filter_dim),
+    num_filters_(num_filters),
+    weights({dim_.x, dim_.y, dim_.z*num_filters}),
+    biases_(num_filters),
     stride(stride)
     {
         std::srand(std::chrono::steady_clock::now().time_since_epoch().count());
@@ -23,7 +32,7 @@ public:
     }
 
     void setBias(unsigned channel, float val){
-        biases_[biases_.slice(channel)] = val;
+        biases_[channel] = val;
     }
 
     float getBias(unsigned channel){
@@ -37,58 +46,88 @@ public:
     SimpleMatrix<float> padInput() const{
         // Create the augmented input data
         SimpleMatrix<float> padded({
-            input_data_->dim(0) + 2*weights.dim(0) - 1,
-            input_data_->dim(1) + 2*weights.dim(1) - 1,
-            weights.dim(2)
+            input_data_->dim(0) + 2*dim_.x - 1,
+            input_data_->dim(1) + 2*dim_.y - 1,
+            input_data_->dim(2)
         });
 
-        padded[padded.subMatIdx({weights.dim(0), weights.dim(1), 0}, input_data_->dims())] = *input_data_;
+        padded[padded.subMatIdx({dim_.x, dim_.y, 0}, input_data_->dims())] = *input_data_;
         return padded;
     }
 
-    SimpleMatrix<float> convolve(){
+    [[nodiscard]] static float activate(const float val, ModelActivationFunction type = RELU) noexcept {
+        switch (type){
+            default:
+            case RELU:
+                return (val < 0 ? 0 : val);
+
+            case SIGMOID:
+                return 1.0f/(1 + expf(val));
+
+            case LINEAR:
+                return val;
+
+            case TANGENT:
+                return tanh(val);
+                
+            case LEAKY_RELU:
+                return std::max(0.1f*val, val);
+        }
+    }
+
+    [[nodiscard]] SimpleMatrix<float> convolve(){
         if (not input_data_) 
             throw std::runtime_error("No input data provided");
 
         const auto input_channel_count = input_data_->dim(2);
-        if (weights.dim(2) % input_channel_count)
+        if (input_channel_count != dim_.z)
             throw std::out_of_range("Mismatched channel count for convolution");
 
-        const auto channel_count = weights.dim(2) / input_channel_count;
         SimpleMatrix<float> input_augmented = padInput();
 
         // Create the output container
         SimpleMatrix<float> output({
-            input_augmented.dim(0) - weights.dim(0)+1, 
-            input_augmented.dim(1) - weights.dim(1)+1, 
-            channel_count
+            input_augmented.dim(0) - dim_.x + 1, 
+            input_augmented.dim(1) - dim_.y + 1, 
+            num_filters_
         });
 
-        const dim3 channel_strip{1, 1, channel_count};
+        const dim3 sub_region_size(dim_.x, dim_.y, input_channel_count);
         dim3 idx{0, 0, 0};
         for (idx.x = 0; idx.x < (input_augmented.dim(0) - weights.dim(0)); idx.x++){
             for (idx.y = 0; idx.y < (input_augmented.dim(1) - weights.dim(1)); idx.y++){
                 // Extract the sub region
-                SimpleMatrix<float> sub_region = input_augmented.subMatCopy(idx, weights.dims());
-                // Multiply by weights (all channels at once)
-                sub_region *= weights;
-                // Add the biases
-                sub_region += biases_;
-                // Set the region in the output layer
-                output[output.subMatIdx(idx, channel_strip)] = sub_region.channelSum();
+                SimpleMatrix<float> sub_region = input_augmented.subMatCopy(idx, sub_region_size);
+
+                // For each filter layer, apply the convolution process to this sub-region
+                for (uint filter_layer = 0; filter_layer < num_filters_; filter_layer++){
+                    // Get the index for the weights that need to be applied to this layer
+                    auto layer_weight_idx = weights.slices(filter_layer*dim_.z, dim_.z);
+                    // Multiply by the weights
+                    SimpleMatrix<float> layer_sub_region = sub_region;
+                    layer_sub_region *= weights[layer_weight_idx];
+                    // Sum the resulting matrices and add the biases
+                    float z_val = layer_sub_region.sum() + biases_[filter_layer];
+                    // Apply the activation function
+                    z_val = activate(z_val, RELU);
+                    // Set the value in the output layer
+                    output(idx.x, idx.y, filter_layer) = z_val;
+                }
             }
         }
 
         return output;
     }
 
-    SimpleMatrix<float> weights;
-    unsigned stride = 1;
-
 private:
     dim3 dim_;
-    SimpleMatrix<float> biases_;
+    unsigned num_filters_ = 0;
+    std::vector<float> biases_;
     const SimpleMatrix<float>* input_data_;
+
+public:
+    SimpleMatrix<float> weights;
+    unsigned stride = 1;
 };
 
 } // namespace my_cnn
