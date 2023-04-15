1830f0cd4218184d66308799b023a760252df12c
alexnavtt <alexnavtt@utexas.edu>
2023-04-14 19:28:25 -0500

    Implemented index memory and used in backprop

--- include/cnn_from_scratch/Pooling.h
+++ include/cnn_from_scratch/Pooling.h
@@ -14,32 +14,48 @@ enum PoolingType{
     
 class Pooling : public ModelLayer{
 public:
-    unsigned dim0 = 1;
-    unsigned dim1 = 1;
-    unsigned stride0 = 1;
-    unsigned stride1 = 1;
-    PoolingType type = PoolingType::MAX;
+    Pooling(dim2 dim, dim2 stride, PoolingType type = MAX):
+    dim_(dim), stride_(stride), type_(type)
+    {}
 
-    bool checkSize(const SimpleMatrix<float>& input){
-        return input.dim(0) >= dim0 && input.dim(1) >= dim1;
+    bool checkSize(const SimpleMatrix<float>& input) override {
+        return input.dim(0) >= dim_.x && input.dim(1) >= dim_.y;
     }
 
     SimpleMatrix<float> propagateForward(const SimpleMatrix<float>& input) override {
-        SimpleMatrix<float> output({input.dim(0)/dim0, input.dim(1)/dim1, input.dim(2)});
-        const dim3 pool_size{dim0, dim1, 1};
+        // Create the approriately sized output
+        SimpleMatrix<float> output({input.dim(0)/dim_.x, input.dim(1)/dim_.y, input.dim(2)});
+
+        // Reset the affected indices vector
+        affected_indices_.clear();
+        affected_indices_.reserve(output.dim(0) * output.dim(1) * output.dim(2));
+
+        const dim3 pool_size{dim_.x, dim_.y, 1};
         for (unsigned z = 0; z < input.dim(2); z++){
             dim3 in_idx{0, 0, z};
             dim3 out_idx{0, 0, z};
-            for (in_idx.y = 0, out_idx.y = 0; in_idx.y < input.dim(1) - dim1 + 1; in_idx.y += stride1, out_idx.y++){
-                for (in_idx.x = 0, out_idx.x = 0; in_idx.x < input.dim(0) - dim0 + 1; in_idx.x += stride0, out_idx.x++){
+
+            for (in_idx.y = 0, out_idx.y = 0; in_idx.y < input.dim(1) - dim_.y + 1; in_idx.y += stride_.y, out_idx.y++){
+                for (in_idx.x = 0, out_idx.x = 0; in_idx.x < input.dim(0) - dim_.x + 1; in_idx.x += stride_.x, out_idx.x++){
                     const auto AoI = input.subMatCopy(in_idx, pool_size);
-                    switch (type){
+
+                    switch (type_){
                         case MIN:
-                            output(out_idx) = AoI.min();
+                        {
+                            const dim3 min_index = AoI.minIndex();
+                            const dim3 min_index_global = in_idx + min_index;
+                            const size_t affected_index = input.getIndex(min_index_global);
+                            affected_indices_.push_back(affected_index);
+                            output(out_idx) = AoI[affected_index];
                             break;
+                        }
 
                         case MAX: 
-                            output(out_idx) = AoI.max();
+                            const dim3 max_index = AoI.maxIndex();
+                            const dim3 max_index_global = in_idx + max_index;
+                            const size_t affected_index = input.getIndex(max_index_global);
+                            affected_indices_.push_back(affected_index);
+                            output(out_idx) = AoI[affected_index];
                             break;
 
                         case AVG:
@@ -53,9 +69,19 @@ public:
     }
 
     SimpleMatrix<float> propagateBackward(const SimpleMatrix<float>& input, const SimpleMatrix<float>& output_grad, [[maybe_unused]] float learning_rate) override{
-        /** TODO: */
-        return input;
+        SimpleMatrix<float> dLdz(input.dims(), 0);
+        size_t out_idx = 0;
+        for (size_t in_idx : affected_indices_){
+            dLdz[in_idx] = output_grad[out_idx++];
+        }
+        return dLdz;
     }
+
+private:
+    dim2 dim_;
+    dim2 stride_;
+    PoolingType type_;
+    std::vector<size_t> affected_indices_;
 };
 
 } // namespace my_cnn
