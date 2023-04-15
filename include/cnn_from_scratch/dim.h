02166714d90a4b0f7e7dd823c68da610418543f1
alexnavtt <alexnavtt@utexas.edu>
2023-04-14 19:27:59 -0500

    Renamed to dim and added a dim2 class

--- include/cnn_from_scratch/dim.h
+++ include/cnn_from_scratch/dim.h
@@ -3,6 +3,27 @@
 #include <ostream>
 
 namespace my_cnn{
+
+struct dim2{
+    union{
+        struct{
+            unsigned x;
+            unsigned y;
+        };
+        unsigned data[2] = {0, 0};
+    };
+
+    dim2() = default;
+    dim2(unsigned val) : x(val), y(val) {}
+    dim2(unsigned x_, unsigned y_) : x(x_), y(y_) {}
+
+    friend std::ostream& operator << (std::ostream& os, dim3 dim){
+        os << "(" << dim.x << ", " << dim.y << ")";
+        return os;
+    }
+    bool operator==(const dim2& other) const{return (x == other.x) && (y == other.y);}
+    bool operator!=(const dim2& other) const{return not (other == *this);}
+};
     
 struct dim3{
     union{
@@ -24,6 +45,7 @@ struct dim3{
     }
     bool operator==(const dim3& other) const{return (x == other.x) && (y == other.y) && (z == other.z);}
     bool operator!=(const dim3& other) const{return not (other == *this);}
+    dim3 operator+(const dim3& other) const{return dim3(x + other.x, y + other.y, z + other.z);}
     dim3 slice() const noexcept {return {x, y, 1};}
 };
 
