e8f6704c26a9860d02393aba976baf91a7e7576d
alexnavtt <alexnavtt@utexas.edu>
2023-04-17 17:16:44 -0500

    Fixed backprop patrix size mismatch

--- include/cnn_from_scratch/ConnectedLayer.h
+++ include/cnn_from_scratch/ConnectedLayer.h
@@ -44,7 +44,7 @@ public:
     }
 
     SimpleMatrix<float> propagateBackward(const SimpleMatrix<float>& input_data, const SimpleMatrix<float>& output_grad, float learning_rate) override{
-        const SimpleMatrix<float>   dLdW = matrixMultiply(output_grad, input_data);
+        const SimpleMatrix<float>   dLdW = matrixMultiply(output_grad, transpose(input_data));
         const SimpleMatrix<float>&  dLdB = output_grad;
         const SimpleMatrix<float>&& dXdZ = matrixMultiply(weights.transpose(), output_grad);
         weights -= learning_rate * dLdW;
