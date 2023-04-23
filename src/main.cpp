bebc0efdbd063c5ed5c2ec9b5c3b30d173c94958
alexnavtt <alexnavtt@utexas.edu>
2023-04-23 12:04:48 -0500

    Implemented structure to large scale training

--- src/main.cpp
+++ src/main.cpp
@@ -19,17 +19,12 @@ int main(int argc, char* argv[]){
 
     my_cnn::MNISTReader db
         ("../data/MNIST Train Images.ubyte-img", 
-        "../data/MNIST Train Labels.ubyte-label");
-
-    // Grayscale image for testing
-    my_cnn::SimpleMatrix<unsigned char> input_image = db.nextImage().data;
-    input_image = db.nextImage().data;
-    
+        "../data/MNIST Train Labels.ubyte-label");        
 
     // Create a model to put the image through
     my_cnn::ModelDescription<unsigned char, unsigned char> model;
 
-    // Create an edge detection kernel
+    // Create a kernel 5x5 kernel with 2 output channels and a stride of 1
     auto K1 = std::make_shared<my_cnn::Kernel>(my_cnn::dim3(5, 5, 1), 2, 1);
     K1->activation = my_cnn::RELU;
 
@@ -48,16 +43,31 @@ int main(int argc, char* argv[]){
     model.addConnectedLayer(10, "ConnectedLayer");
     model.setOutputLabels({0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
 
-    for (int i = 0; i < 1; i++){
-        global_timer.tic("forwardPropagation");
-        my_cnn::ModelResults result = model.forwardPropagation(input_image, &model.output_labels[4]);
-        global_timer.toc("forwardPropagation");
+    int batch_size = 1;
+    for (int j = 0; j < 2; j++){
+        int correct_count = 0;
+        for (int i = 0; i < batch_size; i++){
+            global_timer.tic("getImage");
+            auto Data = db.nextImage();
+            global_timer.toc("getImage");
+
+            my_cnn::ModelResults result = model.forwardPropagation(Data.data, &Data.label);
+            model.backwardsPropagation(result, 0.01);
+
+            correct_count += result.label == Data.label;
+        }
+        std::cout << ": Accuracy was " << 100.0*correct_count/batch_size << "%\n";
     }
+
+    auto Data = db.nextImage();
+    auto result = model.forwardPropagation(Data.data);
+    std::cout << "This image was predicted to be a " << +result.label << "\n";
+    my_cnn::printImage(Data.data);
+
     global_timer.summary();
 
     // std::cout << "Model predicted that the image was a " << result.label << "\n";
 
-    // model.backwardsPropagation(result, 0.05);
 
     return 0;
 }
